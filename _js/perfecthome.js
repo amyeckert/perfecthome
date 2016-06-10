e25d4d07040ac4bb0372845bf1995bd5db84f91f
Amy Eckert <amy@amyeckertphoto.com>
2016-06-10 09:43:38 -0400

    initial commit perfecthome

---
+++ _js/perfecthome.js
@@ -0,0 +1,20 @@
+// http://www.marketleader.com/blog/2013/10/31/write-listing-descriptions-that-sell
+
+var myNoun;
+var myAdjective;
+var myVerb;
+var my Adverb;
+
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  // Your code below this line
+    result = "The " + myAdjective + " " + myNoun + " " + myVerb  + " " +  myAdverb + " down the hill."
+
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly"); 
