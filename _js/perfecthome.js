467562b468a945c7c8b09870f6a571eb242c1921
Amy Eckert <amy@amyeckertphoto.com>
2016-06-20 14:37:28 -0400

    3 listings all working with random selection. Need form controls still

--- _js/perfecthome.js
+++ _js/perfecthome.js
@@ -4,8 +4,6 @@ $(document).ready(function(e) {
 	
 	// HIDE QUESITONS, STORY, AND REPLAY BUTTON ----------------//
 	$(".enter-name").show();
-	// $(".questions2").show();
- // 	$(".questions3").show();
 
 	$(".questions1").hide();
 	$(".questions2").hide();
@@ -17,8 +15,6 @@ $(document).ready(function(e) {
 	$(".replay").hide();
 	$("#btn-reset").hide();
 	
-
-
 	// RANDOMLY CHOOSE WHICH LISTING --------------------//
 
 	$("#btn-next").click(function(e) {
@@ -31,33 +27,33 @@ $(document).ready(function(e) {
 	 	$(".enter-name").hide();
 	 	$(".firstName").empty().append($("input.firstName").val());
 	 	// $(".questions1").show(); 
-	 	$(".questions2").show();
+	 	// $(".questions2").show();
  		// $(".questions3").show();
 
-	 	// var recentChoice;
-	 	// var listings = [q1, q2, q3];
+	 	var recentChoice;
+	 	var listings = [q1, q2, q3];
 
-	 	// var chosen = listings[Math.floor(Math.random() * listings.length)];
+	 	var chosen = listings[Math.floor(Math.random() * listings.length)];
 
 	 	//need code to check to see what previous selection was, so that it doesn't repeat in it's randomness.
 
-		// if (chosen == q1) {
-	 // 		$(".questions1").show(); 
-	 // 		$(".questions2").hide();
-		// 	$(".questions3").hide();
-	 // 	}
-		//  else if (chosen == q2) {
-	 // 		$(".questions2").show();
-	 // 		$(".questions1").hide();  
-	 // 		$(".questions3").hide();
-	 // 	}
-		// else { 
-		// 	$(".questions3").show();
-	 // 		$(".questions1").hide(); 
-	 // 		$(".questions2").hide();
-		// };
+		if (chosen == q1) {
+	 		$(".questions1").show(); 
+	 		$(".questions2").hide();
+			$(".questions3").hide();
+	 	}
+		 else if (chosen == q2) {
+	 		$(".questions2").show();
+	 		$(".questions1").hide();  
+	 		$(".questions3").hide();
+	 	}
+		else { 
+			$(".questions3").show();
+	 		$(".questions1").hide(); 
+	 		$(".questions2").hide();
+		};
  
-		// console.log(chosen);
+		console.log(chosen);
 
 	});
 
@@ -73,43 +69,31 @@ $(document).ready(function(e) {
 	    
 	   		// grab the values from the input boxes, then append them to the DOM
 		    $(".adj1").empty().append($("input.adj1").val());
-
 		    $(".favCountry").empty().append($("input.favCountry").val());
-
 		    $(".bestie").empty().append($("input.bestie").val());
-
 		    $(".adj2").empty().append($("input.adj2").val());
 		    
 		    //	if adj2 starts with a vowel, do this
 		    // else append "n "
 
 		    $(".noun1").empty().append($("input.noun1").val());
-
-		    $(".noun2").empty().append($("input.noun2").val());
-
+			$(".noun2").empty().append($("input.noun2").val());
 		    $(".favCartoon").empty().append($("input.favCartoon").val());
-
 		    $(".prez").empty().append($("input.prez").val());
-
 		    $(".gem").empty().append($("input.gem").val());
-
 		    $(".basement").empty().append($("input.basement").val());
-
 		    $(".tree").empty().append($("input.tree").val());
-
 		    $(".artist").empty().append($("input.artist").val());
-
 		    $(".water").empty().append($("input.water").val());
-
 		    $(".num1").empty().append($("input.num1").val());
-
 		    $(".favAnimal").empty().append($("input.favAnimal").val());
-
 		    $(".verb1").empty().append($("input.verb1").val());
 
 		    //	show the listing
 		    $(".listings").show();
 	    	$("#list1").show();
+	    	$("#list2").hide();
+			$("#list3").hide();
 
 	    	//	change button to replay button
 	    	$("#btn-next").hide();
@@ -129,13 +113,10 @@ $(document).ready(function(e) {
 
 	  	$("#btn-submit2").click(function(e) {
 	    	e.preventDefault()
-	    	console.log("listing 2 submit button works")
 
 			var dir = $("#direction option:selected" ).text();
 			var landmark = $("#landmark :selected").text();
-			console.log(dir, landmark);
-			 
-
+			
 	    	$(".num5").empty().append($("input.num5").val());
 	 	    $(".adj3").empty().append($("input.adj3").val());
 	    	$(".num2").empty().append($("input.num2").val());
@@ -151,7 +132,9 @@ $(document).ready(function(e) {
 
 	    	//	show the listing
 	    	$(".listings").show();
+	    	$("#list1").hide();
 	    	$("#list2").show();
+			$("#list3").hide();
 
 	    	//	change button to replay button
 	    	$("#btn-next").hide();
@@ -161,24 +144,50 @@ $(document).ready(function(e) {
 	    	$(".questions2").hide();
 			$("#btn-reset").show();
 
-			console.log(" questionaire 2 working");
 		    //change the h1 message
 		   	var message = document.querySelector(".message").innerHTML = "This one says YOU all over it!";
 
 		});
 
-		// $("#btn-submit3").click(function(e) {
-	 //    	e.preventDefault()
-	 //    	console.log("listing 3 submit button works")
+		$("#btn-submit3").click(function(e) {
+	    	e.preventDefault()
 
-	 		// var message = document.querySelector(".message").innerHTML = "Welcome home!";
-		// });
+	    	var time = $("#time option:selected" ).text();
 
+	    	$(".num6").empty().append($("input.num6").val());
+	 	    $(".num7").empty().append($("input.num7").val());
+	    	$(".disaster").empty().append($("input.disaster").val());
+	    	$(".clothing").empty().append($("input.clothing").val());
+	    	$(".mood").empty().append($("input.mood").val());
+	    	$(".adj6").empty().append($("input.adj6").val());
+	    	$(".adj7").empty().append($("input.adj7").val());
+	 	    $(".urban").empty().append($("input.urban").val());
+	    	$(".room2").empty().append($("input.room2").val());
+	    	$(".verb2").empty().append($("input.verb2").val());
+	    	$(".time").empty().append(time);
+	    	$(".pluAnimals").empty().append($("input.pluAnimals").val());
+	    	$(".verb3").empty().append($("input.verb3").val());
+			
+			//	show the listing
+	    	$(".listings").show();
+	    	$("#list1").hide();
+	    	$("#list2").hide();
+	    	$("#list3").show();
 
+	    	//	change button to replay button
+	    	$("#btn-next").hide();
+	    	$(".replay").show();
+
+	    	// hide the questions
+	    	$(".questions3").hide();
+			$("#btn-reset").show();
+
+	 		var message = document.querySelector(".message").innerHTML = "Bring your toolbox!";
+		});
 
 	// REPLAY BUTTON------------------------//
 	$("#btn-reset").click(function(e) {
-		console.log("click part is working");
+
 		$("#btn-reset").hide();
 		$(".listings").hide();
 
@@ -188,8 +197,12 @@ $(document).ready(function(e) {
 		var message = document.querySelector(".message").innerHTML = "Help us find your perfect home!";
 
 		 //clear inputs
-		 // $(":input").val(" ");
+		 $(":input").val(" ");
+		 $("#btn-next").val("Let's go!");
+		 $("#btn-submit1").val("Call the movers!");
+		 $("#btn-submit2").val("Get packing!");
+		 $("#btn-submit3").val("Call the bank!");
+
 		});	 
 	});
-
 });
