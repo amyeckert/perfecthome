01b9ea3f8a2bc204e40284ac0628877e33da13b3
Amy Eckert <amy@bluecadet.com>
2016-08-29 08:54:57 -0400

    php galore

--- inc/Madlib.class.php
+++ inc/Madlib.class.php
@@ -5,34 +5,36 @@ session_start();
 
 <?php
 class Madlib {
-  
-	public $id = '';
+  //main components of all Madlibs:
+	public $id = ''; 
 	public $questionaire = array ();
-  public $listing = '';
-
+  public $listing = ''; 
+  //build it:
  	public function __construct($id) { //will be 1, 2 or 3 
 	 		$this->id = $id;
 
- 	}																																// last 3 are optional when defaults set here in param
+ 	}	
+  // add a question to the questionaire 
+  //                                                            (last 3 are optional when defaults set in param)
  	public function addQuestion($qid, $label, $placeholder, $weight, $options = null, $validation = '', $process = '') {
- 		// adds above parameters as an array inside $qustionaire array, [] is shorcut.
+ 		// adds above parameters as an array inside $qustionaire array, [] is shorcut to so this.
  		$this->questionaire[$weight] = array(
  			'qid' => $qid, //key
- 			'title' => $lable, //input lable
- 			'placeholder' => $placeholder,
- 			'weight' => $weight, //order within the form
- 			'options' => $options, //for select list options
+ 			'title' => $label, //input label
+ 			'placeholder' => $placeholder, //placeholder text for html input.
+ 			'weight' => $weight, //order within the form, is an array.
+ 			'options' => $options, //for select from list options
  			'validation' => $validation, //validation function 
  			'process' => $process, //process function for fixing grammar, etc. 
  			); 
 
  	}
- 	public function addListing($text) { //$text is long string defind in instance
+ 	public function addListing($text) { //$text is long string defind in each instance, form inputs are assembled.
     $this->listing = $text;
  	}
 
  	public function getQuestion($qid) { 
-    foreach($this->questionaire as $question) { //loop through $questionaire array keys called 'qid', store each as $quesiton.
+    foreach($this->questionaire as $question) { //loop through $questionaire array keys called 'qid', store each as $question.
       if($question['qid'] == $qid) { //if array key of 'qid' == $qid passed to function...
         return $question;
       }
